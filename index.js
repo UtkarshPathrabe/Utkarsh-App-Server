838655ab2695c05bb169cba28a19685080b1ab9b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-10-21 20:12:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import aiImageGenerationDalleRoutes from './apps/ai_image_generation/routes/dalle.route.js';
+import aiImageGenerationPostsRoutes from './apps/ai_image_generation/routes/posts.route.js';
+import aiCodeGeneratorGptRoutes from './apps/ai_code_generator/routes/gpt.route.js';
+import portfolioHackerrankRoutes from './apps/portfolio/routes/hackerrank.route.js';
+import portfolioLeetcodeRoutes from './apps/portfolio/routes/leetcode.route.js';
+import pricewiseProductRoutes from './apps/pricewise/routes/product.route.js';
+import shirtCustomizerRoutes from './apps/shirt_customizer/routes/dalle.route.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.set('trust proxy', true);
+
+app.use('/api/v1/ai_image_generator', aiImageGenerationDalleRoutes);
+app.use('/api/v1/ai_image_generator/posts', aiImageGenerationPostsRoutes);
+app.use('/api/v1/ai_code_generator', aiCodeGeneratorGptRoutes);
+app.use('/api/v1/portfolio/hackerrank', portfolioHackerrankRoutes);
+app.use('/api/v1/portfolio/leetcode', portfolioLeetcodeRoutes);
+app.use('/api/v1/pricewise/products', pricewiseProductRoutes);
+app.use('/api/v1/shirt_customizer', shirtCustomizerRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World! Node JS application for my <a href="https://utkarshpathrabe.com/">Personal Website</a> and other personal projects.');
+});
+
+app.get('*', function (req, res) {
+	res.send('Invalid URL');
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log('Server has started on port ' + PORT);
+});
